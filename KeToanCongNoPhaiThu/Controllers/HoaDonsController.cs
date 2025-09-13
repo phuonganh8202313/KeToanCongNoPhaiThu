8280342d55d4fe8cc06ff8fdb72ccffe0627f15a
phuonganh8202313 <phamthiphuonganh2507@gmail.com>
2025-09-13 11:54:07 +0700

    Sửa view Hóa đơn, thêm controller Hóa đơn

---
+++ KeToanCongNoPhaiThu/Controllers/HoaDonsController.cs
@@ -0,0 +1,122 @@
+﻿using KeToanCongNoPhaiThu.Data;
+using KeToanCongNoPhaiThu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace KeToanCongNoPhaiThu.Controllers
+{
+    public class HoaDonsController : Controller
+    {
+        private readonly ReceivableDbContext _context;
+
+        public HoaDonsController(ReceivableDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HoaDons
+        public IActionResult Index()
+        {
+            var list = _context.HoaDons.ToList();
+            return View(list);
+        }
+        // GET: HoaDons/Create
+        public IActionResult Create()
+        {
+            ViewData["MaKH"] = new SelectList(_context.KhachHangs, "MaKH", "TenKH");
+            ViewData["TrangThaiList"] = new SelectList(new[] { "Đã thanh toán", "Chưa thanh toán", "Thanh toán một phần" });
+            return View();
+        }
+
+        // POST: HoaDons/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(HoaDon hoaDon)
+        {
+            // Kiểm tra trùng MaHD
+            if (_context.HoaDons.Any(h => h.MaHD == hoaDon.MaHD))
+            {
+                ModelState.AddModelError("MaHD", "Mã hóa đơn đã tồn tại");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.HoaDons.Add(hoaDon);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index)); // thành công thì về Index
+                }
+                catch (Exception ex)
+                {
+                    // Nếu có lỗi DB (ví dụ: format sai, null FK…)
+                    ModelState.AddModelError("", "Không thể thêm hóa đơn: " + ex.Message);
+                }
+            }
+
+
+            // Nếu có lỗi thì load lại dropdown
+            ViewData["MaKH"] = new SelectList(_context.KhachHangs, "MaKH", "TenKH", hoaDon.MaKH);
+            ViewData["TrangThaiList"] = new SelectList(
+                new[] { "Đã thanh toán", "Chưa thanh toán", "Thanh toán một phần" }, hoaDon.TrangThai
+            );
+
+            return View(hoaDon);
+        }
+
+
+        // GET: HoaDons/Edit/5
+        public IActionResult Edit(string id)
+        {
+            var hd = _context.HoaDons.Find(id);
+            if (hd == null) return NotFound();
+            return View(hd);
+        }
+
+        // POST: HoaDons/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(HoaDon hoaDon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(hoaDon);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hoaDon);
+        }
+
+        // GET: HoaDons/Delete/5
+        public IActionResult Delete(string id)
+        {
+            var hd = _context.HoaDons.Find(id);
+            if (hd == null) return NotFound();
+            return View(hd);
+        }
+
+        // POST: HoaDons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var hd = _context.HoaDons.Find(id);
+            if (hd != null)
+            {
+                _context.HoaDons.Remove(hd);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: HoaDons/Details/5
+        public IActionResult Details(string id)
+        {
+            var hd = _context.HoaDons.FirstOrDefault(x => x.MaHD == id);
+            if (hd == null) return NotFound();
+            return View(hd);
+        }
+    }
+}
--- KeToanCongNoPhaiThu/Models/HoaDon.cs
+++ KeToanCongNoPhaiThu/Models/HoaDon.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KeToanCongNoPhaiThu.Models
@@ -6,18 +7,23 @@ namespace KeToanCongNoPhaiThu.Models
     public class HoaDon
     {
         [Key]
+        [Required(ErrorMessage = "Mã hóa đơn không được để trống")]
         public string MaHD { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime NgayLap { get; set; }
         public decimal TongTien { get; set; }
         public string TrangThai { get; set; }
 
         [ForeignKey("KhachHang")]
         public string MaKH { get; set; }
+        [ValidateNever]
         public KhachHang KhachHang { get; set; }
 
-        public ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; }
-        public ICollection<ThanhToan> ThanhToans { get; set; }
+        [ValidateNever]
+        public ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; } = new List<ChiTietHoaDon>();
+        [ValidateNever]
+        public ICollection<ThanhToan> ThanhToans { get; set; } = new List<ThanhToan>();
 
     }
 }
